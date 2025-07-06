b1f7819fe1cd47e5c3e5061aeb0a3fe4e4229078
{TTheCharlesChristy} <{charleschristy325@gmail.com}>
2025-07-06 23:47:59 +0100

    Added the links

---
+++ app/components/MainNavigationGrid/MainNavigationGrid.js
@@ -0,0 +1,14 @@
+startReactionTimer = () => {
+    // Logic to start the Reaction Timer game
+    window.location.href = "reaction-timer"; // Redirect to the Reaction Timer game page
+}
+
+startWheelGame = () => {
+    // Logic to start the Wheel Game
+    window.location.href = "wheel-game"; // Redirect to the Wheel Game page
+}
+
+startQuizGame = () => {
+    // Logic to start the Quiz Game
+    window.location.href = "quiz-game"; // Redirect to the Quiz Game page
+}
